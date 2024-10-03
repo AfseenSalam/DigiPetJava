03e2028bfd3997dffe45859c8d73841ebc05c210
Afseen Salam <afseenfazil@gmail.com>
2024-10-03 15:00:09 -0400

    Completed

--- digipetjava/src/main/java/co/grandcircus/digipetjava/AccountController.java
+++ digipetjava/src/main/java/co/grandcircus/digipetjava/AccountController.java
@@ -4,9 +4,14 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -24,10 +29,17 @@ public class AccountController {
     public List<Account> getAccount() {
         return this.accountRepo.findAll();
     }
-
+    @GetMapping("/accounts/{id}")
+    public Account getById(@PathVariable("id") Long id) {
+        return this.accountRepo.findById(id).orElse(null);
+    }
+    @GetMapping("/accounts/by-key/{apiKey}")
+    public Account getByApiKey(@PathVariable("apiKey") String apiKey) {
+        return this.accountRepo.findByApiKey(apiKey);
+    }
     
      @PostMapping("/accounts")
-    public Account addCar(@RequestBody Account entity) {
+    public Account addAccount(@RequestBody Account entity) {
        
         UUID uuid = UUID.randomUUID();
         entity.setApiKey(uuid.toString());
@@ -35,5 +47,54 @@ public class AccountController {
         accountRepo.save(entity);
         return entity;
     }
+    @DeleteMapping("/accounts/{id}")
+    public void DeleteAccount(@PathVariable ("id") Long id){
+        this.accountRepo.deleteById(id);
+    }
+    @PutMapping("/accounts/{id}/add-credits")
+    public Account UpdateAccount( @PathVariable Long id ,int amount){
+        Account UpdateAccount = accountRepo.findById(id).orElse(null);
+        UpdateAccount.setCredits(UpdateAccount.getCredits()+amount);
+        this.accountRepo.save(UpdateAccount);
+        return UpdateAccount;
+    }
+    @PostMapping("/accounts/{id}/action")
+    public Account postByAction(@PathVariable Long id,@RequestParam String action) {
+        Account UpdateAccount = accountRepo.findById(id).orElse(null);
+        int newCredit =0;
+        if(action == "CREATE"){
+            newCredit=UpdateAccount.getCredits()-5;
+        }
+        else if(action == "HEAL"){
+           newCredit = UpdateAccount.getCredits()-1;
+        }else if(action == "TRAIN"){
+            newCredit = UpdateAccount.getCredits()-1;
+         }else if(action == "BATTLE"){
+            newCredit = UpdateAccount.getCredits()-2;
+         }
+         UpdateAccount.setCredits(newCredit);
+         this.accountRepo.save(UpdateAccount);
+        return UpdateAccount;
+    }
+    @PostMapping("/accounts/by-key/{apiKey}/action")
+    public Account postMethodName(@PathVariable("apiKey") String apiKey,@RequestParam String action) {
+        Account UpdateAccount= accountRepo.findByApiKey(apiKey);
+        int newCredit =0;
+        if(action == "CREATE"){
+            newCredit=UpdateAccount.getCredits()-5;
+        }
+        else if(action == "HEAL"){
+           newCredit = UpdateAccount.getCredits()-1;
+        }else if(action == "TRAIN"){
+            newCredit = UpdateAccount.getCredits()-1;
+         }else if(action == "BATTLE"){
+            newCredit = UpdateAccount.getCredits()-2;
+         }
+         UpdateAccount.setCredits(newCredit);
+         this.accountRepo.save(UpdateAccount);
+        return UpdateAccount;
+    }
+    
+    
 
 }
