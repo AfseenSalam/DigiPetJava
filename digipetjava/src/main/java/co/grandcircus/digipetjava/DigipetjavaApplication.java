f1a46e05b7e64cab27aefebe9693a82a57d9715a
Afseen Salam <afseenfazil@gmail.com>
2024-10-03 11:14:04 -0400

    Initial  commit

---
+++ digipetjava/src/main/java/co/grandcircus/digipetjava/DigipetjavaApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.digipetjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigipetjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigipetjavaApplication.class, args);
+	}
+
+}
